96a70ff304a595a6f0ab1dc287d6faba42dd8db7
Marzhan <m.zhylkaidarova@gmail.com>
2021-08-08 22:49:30 +0200

    removed p_env

--- p_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/marzhanzhylkaidarova/Desktop/P_projects/pizzeria/p_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
